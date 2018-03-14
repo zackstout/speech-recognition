db1daa590a6c90341b3350f028445cd5aa1d34d8
Zack Stout <zackstout@gmail.com>
2018-03-14 03:20:51 -0500

    adding response

--- server/public/client.js
+++ server/public/client.js
@@ -2,17 +2,31 @@
 // This is all aaron's code:
 // Wait he was missing parentheses for call?
 let recog = new webkitSpeechRecognition();
-// let ss = speechSynthesis;
-// let welcome = new SpeechSynthesisUtterance('おかえりなさい');
-// let result = document.getElementById('result');
 
-// recog.lang = 'ja-JP';
+
+
+// all right this makes it listen...but not for new things...
+recog.continuous = true;
+recog.interimResults = true;
+
+
+// recog also has .onstart, .onend, and .onerror methods.
+
+let ss = speechSynthesis;
+let welcome = new SpeechSynthesisUtterance('Ahoy there mate-ee');
+// let result = document.getElementById('result');
+// ss.speak(welcome);
+recog.lang = 'es';
 
 
 // Called when speech is done:
 // Omg it really works: That's amazing.
 recog.onresult = function(event) {
-  console.log(event);
+  var text = event.results[0][0].transcript;
+  console.log(text);
+  if (text.includes("Hello")) {
+    ss.speak(welcome);
+  }
   // let text = event.results[0][0].transcript;
   // result.textContent = text;
   // if (text.includes('ただいま')) {
